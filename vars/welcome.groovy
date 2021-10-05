0fd114a829999614dc3efdddc187904c79088fb5
Gavrila <alin.gavrila@accenture.com>
2021-10-05 09:37:59 +0300

    first build

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,18 @@
-def call(name){
-  echo "Hey ${name}, How are you?"
+def build(){
+  mvn package
 }
+
+// def buildImage(){
+//   echo "Build image with tag: ${env.BUILD_ID}"
+//   myapp = docker.build("alingavrila/ledger-service:${env.BUILD_ID}", "--build-arg VERSION='${env.BUILD_ID}' .")
+// }
+
+// def pushImage(){
+//   script {
+//     docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+//       myapp.push("latest")
+//       myapp.push("${env.BUILD_ID}")
+//     }
+//   }
+// }
+
