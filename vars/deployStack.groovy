5a59090abcab8fc6b075841207a6272b1d78f62c
Gavrila <alin.gavrila@accenture.com>
2021-10-11 18:37:44 +0300

    commit

--- vars/deployStack.groovy
+++ vars/deployStack.groovy
@@ -4,7 +4,7 @@ def call(Map stageParams = [awsRegion: "us-east-1"]){
         cfnCreateChangeSet(
           stack:'Stack', 
           changeSet:'my-change-set', 
-          url:'https://mybucketname123321123.s3.amazonaws.com/cloudformation.yaml'
+          url:'https://mybucketname123321123.s3.amazonaws.com/ec2.yaml'
         )
         cfnExecuteChangeSet(
           stack:'Stack', 
