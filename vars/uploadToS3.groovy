842e2c4d4f4a42712e65079aabc73ff491254709
Gavrila <alin.gavrila@accenture.com>
2021-10-11 18:20:11 +0300

    commit

--- vars/uploadToS3.groovy
+++ vars/uploadToS3.groovy
@@ -1,24 +1,9 @@
-def call(Map stageParams) {
-    withAWS(region: stageParams.awsRegion, credentials:'AWS_Lambda') {
+def upload(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "mybucketname123321123"]) {
+    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
-            workingDir: "build",
-            includePathPattern: "**/*",
-            bucket: stageParams.s3Bucket,
-            path: stageParams.s3Path
-        )
-    }
-}
-
-def call(Map stageParams = [Region: "us-east-1",  s3Bucket: "test-bucket-2221", s3Path: "/"]) {
-
-    withAWS(region: stageParams.Region, credentials:'awscredentials') {
-        awsIdentity()
-        s3Upload( 
-            workingDir: workingDir,
-            includePathPattern: path,
-            bucket: s3Bucket,
-            path: s3Path
+            includePathPattern: "*.yaml",
+            bucket: stageParams.s3Bucket
         )
     }
 }
